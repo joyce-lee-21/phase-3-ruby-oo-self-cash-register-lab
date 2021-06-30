35730b41113822313fb37df22d38a457a4355fb2
Joyce Lee <joycelee901@gmail.com>
2021-06-30 09:07:14 -0500

    commit tree fbe812402a1480d45f4fdaea010a75dd82a5b519

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+require "pry"
 
+class CashRegister
+    attr_accessor :total, :discount
+
+    def initialize(total=0, discount=20)
+        @total = total
+        @discount = discount
+    end
+
+    def add_item(title, price, quantity=nil)
+        if quantity
+            @total += quantity * price
+        else
+            @total += price
+        end
+    end
+
+    def apply_discount
+        self.total = self.total - (self.total * (@discount.to_f / 100.to_f))
+        # binding.pry
+        "After the discount, the total comes to $#{@total.to_i}."
+    end
+    
+end
